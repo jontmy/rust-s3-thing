7c8071019cfdc1055d070ab33bcf8581b08252bf
jontmy <jonathantaymy@gmail.com>
2024-04-04 18:24:31 +0800

    feat: add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+extern crate dotenv;
+
 use axum::{routing::get, Router};
+use dotenv::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let port = env::var("PORT").unwrap();
+    let addr = format!("localhost:{port}");
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
